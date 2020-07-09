26227eca2810fc3d8cf605a347323397f6adaddb
Attila Bagossy <bagossyattila@outlook.com>
2020-07-09 11:03:31 +0200

    feat: delegating evaluation calls from DTE to CompiledTemplateEvaluator

---
+++ src/evaluator/CompiledTemplateEvaluator.ts
@@ -0,0 +1,8 @@
+import { CompiledTemplate } from './CompiledTemplate'
+import { EvaluationOptions } from './EvaluationOptions'
+
+export class CompiledTemplateEvaluator {
+    evaluateCompiledTemplate(compiledTemplate: CompiledTemplate, substitutionData: object, options?: EvaluationOptions): string {
+        throw new Error("Method not implemented.");
+    }
+}
--- src/evaluator/DefaultTemplateEvaluator.ts
+++ src/evaluator/DefaultTemplateEvaluator.ts
@@ -1,13 +1,21 @@
 import { TemplateEvaluator } from './TemplateEvaluator'
 import { CompilationOptions } from './CompilationOptions';
 import { CompiledTemplate } from './CompiledTemplate';
+import { CompiledTemplateEvaluator } from './CompiledTemplateEvaluator'
 import { EvaluationOptions } from './EvaluationOptions';
 
 export class DefaultTemplateEvaluator implements TemplateEvaluator {
+    private evaluator: CompiledTemplateEvaluator
+
+    constructor() {
+        this.evaluator = new CompiledTemplateEvaluator()
+    }
+
     compileExpression(template: String, options?: CompilationOptions): CompiledTemplate {
         throw new Error("Method not implemented.");
     }
+
     evaluateCompiledTemplate(compiledTemplate: CompiledTemplate, substitutionData: object, options?: EvaluationOptions): string {
-        throw new Error("Method not implemented.");
+        return this.evaluator.evaluateCompiledTemplate(compiledTemplate, substitutionData, options)
     }
 }
