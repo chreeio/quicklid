8daa2ddcb466bed5372683f1053841df11c93d50
Attila Bagossy <bagossyattila@outlook.com>
2020-07-10 12:25:20 +0200

    test: ability to add negative test cases

--- test/TemplateCompiler.test.ts
+++ test/TemplateCompiler.test.ts
@@ -7,22 +7,56 @@ import { CompiledTemplate } from '../src/evaluator/CompiledTemplate'
 interface TestCase {
     title: string,
     template: string,
-    expected: CompiledTemplate
+    expected?: CompiledTemplate
 }
 
-describe('Compiler with default settings', () => {
-    const testCases: TestCase[] = require('./TemplateCompiler.testcase.json')
+interface SettingGroup {
+    [propertyName: string]: TestCase[]
+}
 
-    testCases.forEach(testCase => {
-        test(testCase.title, () => {
-            // Given
-            const underTest = new TemplateCompiler()
+interface TestCaseFile {
+    [propertyName: string]: SettingGroup
+}
 
-            // When
-            const actual = underTest.compileTemplate(testCase.template, Quicklid.defaultCompilationOptions)
+const testData: TestCaseFile = require('./TemplateCompiler.testcase.json')
 
-            // Then
-            expect(actual).toEqual(testCase.expected)
+function positiveTest(testCase: TestCase) {
+    test(testCase.title, () => {
+        // Given
+        const underTest = new TemplateCompiler()
+
+        // When
+        const actual = underTest.compileTemplate(testCase.template, Quicklid.defaultCompilationOptions)
+
+        // Then
+        expect(actual).toEqual(testCase.expected)
+    })
+}
+
+function negativeTest(testCase: TestCase) {
+    test(testCase.title, () => {
+        // Given
+        const underTest = new TemplateCompiler()
+
+        // Expect
+        expect(() => underTest.compileTemplate(testCase.template, Quicklid.defaultCompilationOptions))
+            .toThrow()
+    })
+}
+
+describe('Compiler', () => {
+    Object.entries(testData).forEach(([settingName, settingGroup]) => {
+        describe(settingName, () => {
+            Object.entries(settingGroup).forEach(([caseGroupName, testCases]) => {
+                describe(caseGroupName, () => {
+                    testCases.forEach(testCase => {
+                        Object.prototype.hasOwnProperty.call(testCase, 'expected')
+                            ? positiveTest(testCase)
+                            : negativeTest(testCase)
+                    })
+                })
+            })
         })
     })
 })
+
